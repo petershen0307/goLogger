fe05b9cc1ee785addbfcf76979682081a43a381d
PeterShen <petershen0307@hotmail.com>
2017-12-26 00:32:14 +0800

    [log server] to catch os signal

--- logServer/server.go
+++ logServer/server.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
 
 	winio "github.com/Microsoft/go-winio"
 )
@@ -27,42 +29,52 @@ func openPipeServer() (listener net.Listener, err error) {
 	return
 }
 
-func pipeReceiver(c chan jobStruct, listener net.Listener) {
+func pipeReceiver(jobQueue chan jobStruct, listener net.Listener) {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println(err)
-			c <- jobStruct{jobCommand: cmdExit, message: ""}
+			jobQueue <- jobStruct{jobCommand: cmdExit, message: ""}
 			return
 		}
 		bufioReader := bufio.NewReader(conn)
 		msg, _ := bufioReader.ReadString('\n')
 		job := jobStruct{jobCommand: cmdMessage, message: msg}
-		c <- job
+		jobQueue <- job
 		conn.Close()
 	}
 }
 
-func worker(c chan jobStruct) {
+func worker(jobQueue chan jobStruct) {
 	for {
-		job := <-c
+		job := <-jobQueue
 		switch job.jobCommand {
 		case cmdFlush:
 		case cmdMessage:
 			fmt.Print(job.message)
 		case cmdSplit:
 		case cmdExit:
+			if 0 != len(jobQueue) {
+				continue
+			}
 			break
 		}
 	}
 }
 
 func main() {
-	c := make(chan jobStruct, 300)
-	ln, err := openPipeServer()
+	jobQueue := make(chan jobStruct, 300)
+	listener, err := openPipeServer()
 	if err != nil {
 		fmt.Println(err)
 	}
-	go pipeReceiver(c, ln)
-	worker(c)
+	go pipeReceiver(jobQueue, listener)
+	go worker(jobQueue)
+	osEvent := make(chan os.Signal, 1)
+	signal.Notify(osEvent, os.Interrupt)
+	for sig := range osEvent {
+		// sig is a ^C, handle it
+		fmt.Println(sig)
+		break
+	}
 }
